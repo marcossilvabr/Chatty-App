4920802c174065768b908678b2f2e6c3f9ef3d44
Marcos Silva <marcosgustavobh@gmail.com>
2017-07-25 19:03:57 +0000

    chatbar now displays inputed user and message!

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,27 +1,38 @@
-
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
-    this.state = {value: ''};
-  }
-  handleChange(e) {
-    if (e.key === 'Enter') {
-      this.state.value = e.target.value;
-      console.log(e.target.value)
-      // invokes function in app.jsx
-      this.props.onSend(e.target.value);
-    }
+    this.handleUsername = this.handleUsername.bind(this);
+    this.state = {
+      username: '',
+      message: ''
+    };
   }
 
+  handleChange(e) {
+    if (e.key === 'Enter') {
+      this.state.message = e.target.value;
+      if (!this.state.username) {
+        this.state.username = 'Anonymous';
+      }
+      this.props.onSend(this.state.message, this.state.username);
+    }
+  }
+  handleUsername(e) {
+    this.state.username = e.target.value
+  }
+
+
   render() {
-    const value = this.state.value;
     return (
 
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
+        <input
+          className="chatbar-username"
+          placeholder="Your Name (Optional)"
+          onKeyUp={this.handleUsername} />
         <input
           className="chatbar-message"
           placeholder="Type a message and hit ENTER"
